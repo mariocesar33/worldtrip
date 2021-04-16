e16b65e5774b6bf05e9d8f88f067129eeb95dd8f
Mário César Silva <mcgato33@hotmail.com>
2021-04-16 15:31:47 -0100

    theme e _document

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,30 +1,30 @@
-// import { extendTheme } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react';
 
-// export const theme = extendTheme({
-//   colors: { 
-//     gray: {
-//       "900": "#181B23",
-//       "800": "#1F2029",
-//       "700": "#353646",
-//       "600": "#4B4D63",
-//       "500": "#616480",
-//       "400": "#797D9A",
-//       "300": "#9699B0",
-//       "200": "#B3B5C6",
-//       "100": "#D1D2DC",
-//       "50": "#EEEEF2",
-//     }
-//   },
-//   fonts: {
-//     heading: 'Roboto',
-//     body: 'Roboto',
-//   },
-//   styles: {
-//     global: {
-//       body: {
-//         bg: 'gray.900',
-//         color: 'gray.50',
-//       }
-//     }
-//   }
-// });
+export const theme = extendTheme({
+  colors: { 
+    gray: {
+      "700": "#47585B",
+      "600": "#999999",
+      "500": "#CCCCCC",
+      "400": "#797D9A",
+      "300": "#DADADA",
+      "100": "#F5F8FA",
+    },
+    yellow: {
+      "400": "#FFBA08",
+      "300": "#FFDB7F",
+    }
+  },
+  fonts: {
+    heading: 'Poppins',
+    body: 'Poppins',
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.100',
+        color: 'gray.700',
+      }
+    }
+  }
+});
